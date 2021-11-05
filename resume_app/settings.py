bbff1d50efaed8e98139e1f1055db1aed0e72d38
Joseph Segbefia <joeelorm@outlook.com>
2021-11-05 18:28:44 +0100

    Second push

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'josephsegbefia.herokuapp.com']
 
 
 # Application definition
