5e944c15d6ba83b4cddf0a3f24ffb0c6bbf8453b
Joseph Segbefia <joeelorm@outlook.com>
2021-11-06 23:14:11 +0100

    sixth commit

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -85,16 +85,11 @@ WSGI_APPLICATION = 'resume_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-}
+DATABASES = {}
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age = 500, ssl_require = True)
-DATABASES['default'].update(db_from_env)
+db_from_env = 
+DATABASES['default'] = dj_database_url.config(conn_max_age = 600, ssl_require = True)
 
 
 # Password validation
