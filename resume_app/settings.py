10d90bd4db2437e38454c3f6e3cd400f4298eb9b
Joseph Segbefia <joeelorm@outlook.com>
2021-11-06 23:24:50 +0100

    sixth commit

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'resume_app.wsgi.application'
 DATABASES = {}
 
 import dj_database_url
-db_from_env = 
+
 DATABASES['default'] = dj_database_url.config(conn_max_age = 600, ssl_require = True)
 
 
