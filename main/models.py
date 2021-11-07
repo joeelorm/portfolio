63cb24f160db99d6c869a52ba3c4fa00a45100fe
Joseph Segbefia <joeelorm@outlook.com>
2021-11-07 15:43:44 +0100

    14th commit fixed ckeditor

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
-from ckeditor.fields import RichTextUploadingField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Skill(models.Model):
